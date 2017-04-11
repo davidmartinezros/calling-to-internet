81ab5277948b7767ab8943434ee49ca21bf5a537
David <david.martinez.etic@gmail.com>
2017-04-11 18:36:45 +0200

    convert to a war application with spring boot

--- src/main/java/org/dmr/CallingToInternetApplication.java
+++ src/main/java/org/dmr/CallingToInternetApplication.java
@@ -2,14 +2,22 @@ package org.dmr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by davidmartinezros on 10/04/2017.
  */
 @SpringBootApplication
-public class CallingToInternetApplication {
+public class CallingToInternetApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallingToInternetApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CallingToInternetApplication.class);
+    }
+	
 }
